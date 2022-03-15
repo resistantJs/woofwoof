920ea704cbd5bc7e53d46935e507b1622af6882d
Jason Jiang <jasonjiang@Kevins-Galaxy-S9.Artcenter.edu>
2022-03-15 09:01:26 -0700

    testsearchbar

---
+++ src/components/GoButton.tsx
@@ -0,0 +1,9 @@
+
+
+export default function GoButton({} : { size: any}){
+    return(
+        <>
+            
+        </>
+    )
+}
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,11 @@
+
+
+export default function SearchBar(){
+    return(
+        <>
+            <section className='fixed top-0 left-0 right-0 flex w-full  justify-around  text-lg bg-orange-500 py-10'>
+
+            </section>
+        </>
+    )
+}
