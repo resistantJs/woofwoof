81ed462c32d132578bde334a244bbd84671772bd
Jason Jiang <jasonjiang@Alexias-iPad.Artcenter.edu>
2022-03-22 11:45:12 -0700

    testexplore

---
+++ src/pages/explore.tsx
@@ -0,0 +1,20 @@
+import BigBanner from '@/components/BigBanner';
+import Nav from '@/components/nav';
+export default function Explore() {
+  return (
+    <>
+      <div className='bg-orange py-20'></div>
+      <BigBanner
+        imgHeight='10rem'
+        featured={true}
+        imgLink='/images/BigBanner.jpg'
+      />
+      <BigBanner
+        imgHeight='6rem'
+        featured={false}
+        imgLink='/images/BigBanner.jpg'
+      />
+      <Nav />
+    </>
+  );
+}
